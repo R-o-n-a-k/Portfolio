def07bae66c2cb1978bc92f8fb6b0533dd368c53
Ronak Patel <70361182+R-o-n-a-k@users.noreply.github.com>
2022-10-23 14:16:01 +0530

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,85 @@
+(function () {
+  "use strict";
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
+
+
+function clickButton(clicked_id)
+  {     
+     if(clicked_id=="educationheader"){
+      document.getElementById("educationheader").style.color="var(--color-tertiary)";
+      document.getElementById("workheader").style.color="var(--color-grey-2)";
+      document.getElementById("education").style.display = 'block';
+      document.getElementById("work").style.display = 'none';
+     }
+     else{
+      document.getElementById("educationheader").style.color="var(--color-grey-2)";
+      document.getElementById("workheader").style.color="var(--color-tertiary)";
+      document.getElementById("work").style.display = 'block';
+      document.getElementById("education").style.display = 'none';
+     }
+  }
+
+// ----------scroll button---------------
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
+
+
+
+
