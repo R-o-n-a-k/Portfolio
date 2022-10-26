08cce5dedf05f08ff8d29401b41d0a01c33e6fb4
Ronak <rroonnaakk7@gmail.com>
2022-10-26 18:58:16 +0530

    scroll animation

--- public/js/app.js
+++ public/js/app.js
@@ -137,39 +137,3 @@ window.addEventListener('load', function () {
   if (textArray.length) setTimeout(type, newTextDelay + 200);
 });
 
-// ----------------my typing end---------------
-
-
-
-/////////navbar////////
-
-const navslide=()=>{
-  const burger=document.querySelector('.burger');
-  const nav=document.querySelector('.nav-links');
-  const navLinks=document.querySelectorAll('.nav-links li');
-
-  burger.addEventListener('click',()=>{
-  
-  // Toggle Nav
-    nav.classList.toggle('nav-active');
-
-    // Animate links
-    navLinks.forEach((link,index)=>{
-      if(link.style.animation){
-        link.style.animation='';
-      }
-      else{
-        link.style.animation= 'navLinkFade 0.5s ease forwards ${index / 7+1.5}s'; 
-      }
-
-    });
-
-    //Burger animation
-    burger.classList.toggle('toggle');
-
-  });
-  
-  
-}
-
-navslide();
---
+++ public/js/scroll.js
@@ -0,0 +1,3 @@
+//gsap scroll animation
+gsap.from('.hero .ronakLogo',{duration:1,y:'-200%',ease:'bounce',delay:.5})
+gsap.from('.nav-links',{duration:3,y:'-250%',ease:'bounce',delay:.5})
