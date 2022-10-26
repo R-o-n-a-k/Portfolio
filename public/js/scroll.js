4e7e435d17d1c31eb901ee61b7077ab5c0831fa1
Ronak <rroonnaakk7@gmail.com>
2022-10-26 19:07:53 +0530

    scroll2

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,3 +1,3 @@
 //gsap scroll animation
-gsap.from('.hero .ronakLogo',{duration:1.5,x:'-170%',ease:'bounce'})
-gsap.from('.nav-links li',{duration:2,y:'-200%',ease:'bounce',delay:.5})
+gsap.from('.hero .ronakLogo',{duration:1,y:'-100%',ease:'bounce'})
+gsap.from('.nav-links li',{duration:2,y:'-205%',ease:'bounce', stagger:.2})
