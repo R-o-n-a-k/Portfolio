2037e2202321eddebca122c57dda0d6a1f0e2c1d
Ronak <rroonnaakk7@gmail.com>
2022-10-30 21:26:13 +0530

    navbar responsive

--- public/js/app.js
+++ public/js/app.js
@@ -137,3 +137,9 @@ window.addEventListener('load', function () {
   if (textArray.length) setTimeout(type, newTextDelay + 200);
 });
 
+
+function onMenuClick() {
+    var navbar = document.getElementById('navigation-bar');
+    var responsive_class_name = 'responsive'
+    navbar.classList.toggle(responsive_class_name)
+}
